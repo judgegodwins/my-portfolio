402d67d05c1023e646796ec81832678e262ea282
Judge Godwins <judgegodwins@gmail.com>
2020-08-16 14:47:56 +0100

    make link open in new tab

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -15,7 +15,7 @@ function TopBar() {
                 <Link to="/contact">
                     <li className="link">CONTACT ME</li>
                 </Link>
-                <a href="https://docs.google.com/document/d/1_w65zw-qnRA3ZCat1BRNnb-DXVD71CeoxLcFAR70Nz0/edit?usp=sharing">
+                <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/document/d/1_w65zw-qnRA3ZCat1BRNnb-DXVD71CeoxLcFAR70Nz0/edit?usp=sharing">
                     <li className="link">RESUME</li>
                 </a>
             </ul>
